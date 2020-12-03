ecfdd9ce791a6449950fd2a4861881a7f45db8a5
Vojtech Simetka <vojtech@simetka.cz>
2020-12-03 21:34:43 +0100

    refactor: test utility function randomBuffer is now pseudorandom (#14)
    * refactor: test utility function randomBuffer is now pseudorandom
    
    * chore: incorporated PR comments

--- test/utils.ts
+++ test/utils.ts
@@ -21,17 +21,29 @@ export function createReadable(input: string): Readable {
 }
 
 /**
+ * Lehmer random number generator with seed (minstd_rand in C++11)
+ * !!! Very fast but not well distributed pseudo-random function !!!
+ *
+ * @param seed Seed for the pseudo-random generator
+ */
+function lrng(seed: number): () => number {
+  return (): number => ((2 ** 31 - 1) & (seed = Math.imul(48271, seed))) / 2 ** 31
+}
+
+/**
  * Utility function for generating random Buffer
  * !!! IT IS NOT CRYPTO SAFE !!!
  * For that use `crypto.randomBytes()`
  *
- * @param length
+ * @param length Number of bytes to generate
+ * @param seed Seed for the pseudo-random generator
  */
-export function randomBuffer(length: number): Buffer {
+export function randomBuffer(length: number, seed = 500): Buffer {
+  const rand = lrng(seed)
   const buf = Buffer.alloc(length)
 
   for (let i = 0; i < length; ++i) {
-    buf[i] = (Math.random() * 0xff) << 0
+    buf[i] = (rand() * 0xff) << 0
   }
 
   return buf
