2526e5eb817fcaf8eee29e114eec53a0e94d382a
nugaon <toth.viktor.levente@gmail.com>
2020-12-13 23:20:28 +0100

    chore: add ephemeral flag to run bee-client

--- test/bee.sh
+++ test/bee.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
-if [ ! -d /tmp/bee ] ; then
-   mkdir -p /tmp/bee/data && \
-   chown $(id -u):999 -R /tmp/bee && \
-   chmod 770 -R /tmp/bee && \
-   echo "password" > /tmp/bee/password
+# --ephemeral -> create ephemeral container for bee-client. Data won't be persisted
+BEE_CONTAINER=`docker container ls -qaf name=bee-test`
+if [ -z $BEE_CONTAINER ] || [ "$1" = "--ephemeral" ] ; then
+  BEE_IMAGE="ethersphere/bee:0.4.0"
+  BEE_PASSWORD="password"
+  CONTAINER_NAME="bee-test"
+  EXTRA_PARAMS=" --name $CONTAINER_NAME"
+  if [ "$1" = "--ephemeral" ] ; then
+    EXTRA_PARAMS=" --rm"
+  fi
+  docker run \
+    -p 127.0.0.1:1635:1635 \
+    -p 127.0.0.1:1634:1634 \
+    -p 127.0.0.1:1633:1633 \
+    --interactive \
+    --tty \
+    $EXTRA_PARAMS \
+    $BEE_IMAGE \
+      start \
+      --password $BEE_PASSWORD \
+      --standalone=true \
+      --swap-enable=false \
+      --debug-api-enable \
+      --cors-allowed-origins="*"
+else
+  docker start -ai $BEE_CONTAINER
 fi
-docker run $@ \
-  -p 1635:1635 \
-  -p 1634:1634 \
-  -p 1633:1633 \
-  --volume /tmp/bee:/bee \
-  --interactive \
-  --tty \
-  "ethersphere/bee:0.4.0" \
-    start \
-    --data-dir /bee/data \
-    --password-file /bee/password \
-    --standalone=true \
-    --swap-enable=false \
-    --cors-allowed-origins="*"
