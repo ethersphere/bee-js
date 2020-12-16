1ba87dab07cf0260be17661b402812c85a4af64e
Attila Gazso <agazso@gmail.com>
2020-12-16 14:28:03 +0100

    feat: add pinning for data (/bytes endpoint)

--- src/modules/pinning.ts
+++ src/modules/pinning.ts
@@ -2,6 +2,7 @@ import { safeAxios } from '../utils/safeAxios'
 
 const fileEndpoint = '/pin/files'
 const collectionEndpoint = '/pin/bzz'
+const bytesEndpoint = '/pin/bytes'
 
 export interface Response {
   message: string
@@ -67,6 +68,38 @@ export async function unpinCollection(url: string, hash: string): Promise<Respon
     method: 'delete',
     responseType: 'json',
     url: `${url}${collectionEndpoint}/${hash}`,
+  })
+
+  return response.data
+}
+
+/**
+ * Pin data with given reference
+ *
+ * @param url  Bee URL
+ * @param hash Bee data reference
+ */
+export async function pinData(url: string, hash: string): Promise<Response> {
+  const response = await safeAxios<Response>({
+    method: 'post',
+    responseType: 'json',
+    url: `${url}${bytesEndpoint}/${hash}`,
+  })
+
+  return response.data
+}
+
+/**
+ * Unpin data with given reference
+ *
+ * @param url  Bee URL
+ * @param hash Bee data reference
+ */
+export async function unpinData(url: string, hash: string): Promise<Response> {
+  const response = await safeAxios<Response>({
+    method: 'delete',
+    responseType: 'json',
+    url: `${url}${bytesEndpoint}/${hash}`,
   })
 
   return response.data
