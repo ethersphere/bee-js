b140c5d67f3dd56636ec74ef41f07f3aabdb739c
nugaon <toth.viktor.levente@gmail.com>
2020-12-17 09:35:49 +0100

    style: linting on new test file

--- test/bee-class.browser.spec.ts
+++ test/bee-class.browser.spec.ts
@@ -2,10 +2,9 @@ import { join } from 'path'
 import { beeUrl } from './utils'
 
 describe('Bee class - in browser', () => {
-
-  beforeAll( async (done) => {
+  beforeAll(async done => {
     const testPage = join(__dirname, 'testpage', 'testpage.html')
-    await page.goto(`file://${testPage}`);
+    await page.goto(`file://${testPage}`)
 
     done()
   })
@@ -24,14 +23,14 @@ describe('Bee class - in browser', () => {
 
       return await window.bee.uploadFiles(files)
     })
-    expect(typeof fileHash).toBe('string')//TODO: write own matcher to check swarm hashes
+    expect(typeof fileHash).toBe('string') //TODO: write own matcher to check swarm hashes
     //pinning
-    const pinResult = await page.evaluate(async (fileHash) => {
+    const pinResult = await page.evaluate(async fileHash => {
       return await window.bee.pinFile(fileHash)
     }, fileHash)
-    expect(pinResult.code).toBe(200)//TODO: write own matcher to handle Bee client response messages
+    expect(pinResult.code).toBe(200) //TODO: write own matcher to handle Bee client response messages
     //unpinning
-    const unpinResult = await page.evaluate(async (fileHash) => {
+    const unpinResult = await page.evaluate(async fileHash => {
       return await window.bee.unpinFile(fileHash)
     }, fileHash)
     expect(unpinResult.code).toBe(200)
