6416396c7ccb029f60a664d422515a890cab9540
Attila Gazso <agazso@gmail.com>
2021-01-11 12:08:59 +0100

    fix: change PR review requests, add comments

--- src/chunk/bmt.ts
+++ src/chunk/bmt.ts
@@ -3,8 +3,23 @@ import { BeeArgumentError } from '../utils/error'
 
 const MAX_CHUNK_PAYLOAD_SIZE = 4096
 const SEGMENT_SIZE = 32
+const SEGMENT_PAIR_SIZE = 2 * SEGMENT_SIZE
 const HASH_SIZE = 32
 
+/**
+ * Calculate a Binary Merkle Tree hash for a chunk
+ *
+ * The BMT chunk address is the hash of the 8 byte span and the root
+ * hash of a binary Merkle tree (BMT) built on the 32-byte segments
+ * of the underlying data.
+ *
+ * If the chunk content is less than 4k, the hash is calculated as
+ * if the chunk was padded with all zeros up to 4096 bytes.
+ *
+ * @param chunkContent Chunk data including span and payload as well
+ *
+ * @returns the keccak256 hash in a byte array
+ */
 export function bmtHash(chunkContent: Uint8Array): Uint8Array {
   const span = chunkContent.slice(0, 8)
   const payload = chunkContent.slice(8)
@@ -22,12 +37,12 @@ function bmtRootHash(payload: Uint8Array): Uint8Array {
 
   // create an input buffer padded with zeros
   let input = new Uint8Array([...payload, ...new Uint8Array(MAX_CHUNK_PAYLOAD_SIZE - payload.length)])
-  while (input.length > HASH_SIZE) {
+  while (input.length !== HASH_SIZE) {
     const output = new Uint8Array(input.length / 2)
 
-    // in each round we hash the pairs together
-    for (let offset = 0; offset < input.length; offset += SEGMENT_SIZE * 2) {
-      const hashNumbers = keccak256.array(input.slice(offset, offset + SEGMENT_SIZE * 2))
+    // in each round we hash the segment pairs together
+    for (let offset = 0; offset < input.length; offset += SEGMENT_PAIR_SIZE) {
+      const hashNumbers = keccak256.array(input.slice(offset, offset + SEGMENT_PAIR_SIZE))
       output.set(hashNumbers, offset / 2)
     }
 
--- src/chunk/span.ts
+++ src/chunk/span.ts
@@ -1,20 +1,29 @@
 import { BeeArgumentError } from '../../src/utils/error'
 
+// we limit the maximum span size in 32 bits to avoid BigInt compatibility issues
+const MAX_SPAN_LENGTH = 2 ** 32 - 1
+
+/**
+ * Create a span for storing the length of the chunk
+ *
+ * The length is encoded in 64-bit little endian.
+ *
+ * @param length The length of the span
+ */
 export function makeSpan(length: number): Uint8Array {
   if (length <= 0) {
     throw new BeeArgumentError('invalid length for span', length)
   }
 
-  // we limit the maximum span size in 32 bits to avoid BigInt compatibility issues
-  if (length > 2 ** 32 - 1) {
-    throw new BeeArgumentError('invalid length (> 2**32 - 1)', length)
+  if (length > MAX_SPAN_LENGTH) {
+    throw new BeeArgumentError('invalid length (> MAX_SPAN_LENGTH)', length)
   }
 
   const span = new Uint8Array(8)
   const dataView = new DataView(span.buffer)
   const littleEndian = true
-
   const lengthLower32 = length & 0xffffffff
+
   dataView.setUint32(0, lengthLower32, littleEndian)
 
   return span
