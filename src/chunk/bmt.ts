5cac9b2b573a2dfbaf7705e889dfada1520787a2
Attila Gazso <agazso@gmail.com>
2021-01-12 08:37:28 +0100

    feat: bmt hasher (#65)
    * feat: bmt hasher
    
    * test: add span calculation, test bmtHash for less than 4096 bytes
    
    * test: fix linter issue
    
    * refactor: move bmt and span to chunk folder
    
    * test: add test with random chunk data
    
    * fix: change PR review requests, add comments

---
+++ src/chunk/bmt.ts
@@ -0,0 +1,53 @@
+import { keccak256 } from 'js-sha3'
+import { BeeArgumentError } from '../utils/error'
+
+const MAX_CHUNK_PAYLOAD_SIZE = 4096
+const SEGMENT_SIZE = 32
+const SEGMENT_PAIR_SIZE = 2 * SEGMENT_SIZE
+const HASH_SIZE = 32
+
+/**
+ * Calculate a Binary Merkle Tree hash for a chunk
+ *
+ * The BMT chunk address is the hash of the 8 byte span and the root
+ * hash of a binary Merkle tree (BMT) built on the 32-byte segments
+ * of the underlying data.
+ *
+ * If the chunk content is less than 4k, the hash is calculated as
+ * if the chunk was padded with all zeros up to 4096 bytes.
+ *
+ * @param chunkContent Chunk data including span and payload as well
+ *
+ * @returns the keccak256 hash in a byte array
+ */
+export function bmtHash(chunkContent: Uint8Array): Uint8Array {
+  const span = chunkContent.slice(0, 8)
+  const payload = chunkContent.slice(8)
+  const rootHash = bmtRootHash(payload)
+  const chunkHashInput = new Uint8Array([...span, ...rootHash])
+  const chunkHash = keccak256.array(chunkHashInput)
+
+  return Uint8Array.from(chunkHash)
+}
+
+function bmtRootHash(payload: Uint8Array): Uint8Array {
+  if (payload.length > MAX_CHUNK_PAYLOAD_SIZE) {
+    throw new BeeArgumentError('invalid data length', payload)
+  }
+
+  // create an input buffer padded with zeros
+  let input = new Uint8Array([...payload, ...new Uint8Array(MAX_CHUNK_PAYLOAD_SIZE - payload.length)])
+  while (input.length !== HASH_SIZE) {
+    const output = new Uint8Array(input.length / 2)
+
+    // in each round we hash the segment pairs together
+    for (let offset = 0; offset < input.length; offset += SEGMENT_PAIR_SIZE) {
+      const hashNumbers = keccak256.array(input.slice(offset, offset + SEGMENT_PAIR_SIZE))
+      output.set(hashNumbers, offset / 2)
+    }
+
+    input = output
+  }
+
+  return input
+}
