42b1bf48e43634573ff7a7a839c00cab46666ddd
Attila Gazso <agazso@gmail.com>
2021-01-26 18:27:14 +0100

    fix: upload collection with unicode filenames
    Fixes #78

---
+++ src/types/utf8-encoder.d.ts
@@ -0,0 +1,4 @@
+declare module 'utf8-encoder' {
+  function fromString(s: string): Uint8Array
+  function toString(u: Uint8Array): string
+}
--- src/utils/tar.ts
+++ src/utils/tar.ts
@@ -1,12 +1,30 @@
 import { Collection } from '../types'
 import Tar from 'tar-js'
+import * as utf8 from 'utf8-encoder'
 
-export async function makeTar(data: Collection<Uint8Array>): Promise<Uint8Array> {
+// this is a workaround type so that we are able to pass in Uint8Arrays
+// as string to `tar.append`
+interface StringLike {
+  readonly length: number
+  charCodeAt: (index: number) => number
+}
+
+// converts a string to utf8 Uint8Array and returns it as a string-like
+// object that `tar.append` accepts as path
+function fixUnicodePath(path: string): StringLike {
+  const codes = utf8.fromString(path)
+
+  return {
+    length: codes.length,
+    charCodeAt: index => codes[index],
+  }
+}
+
+export function makeTar(data: Collection<Uint8Array>): Uint8Array {
   const tar = new Tar()
   for (const entry of data) {
-    await new Promise<Uint8Array>(resolve =>
-      tar.append(entry.path, entry.data, undefined, (callback: Uint8Array) => resolve(callback)),
-    )
+    const path = fixUnicodePath(entry.path)
+    tar.append(path, entry.data)
   }
 
   return tar.out
