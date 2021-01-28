6d74ea629a7eef1f3ecaa4d9e18b6d8ee38b9e83
Attila Gazso <agazso@gmail.com>
2021-01-28 13:18:31 +0100

    chore: fix linter issues

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -12,14 +12,11 @@ export function prepareData(data: string | Uint8Array | Readable): Uint8Array | 
 export function prepareWebsocketData(data: Data): Uint8Array {
   if (typeof data === 'string') {
     return new TextEncoder().encode(data)
-  }
-  else if (data instanceof Buffer) {
+  } else if (data instanceof Buffer) {
     return new Uint8Array(data)
-  }
-  else if (data instanceof ArrayBuffer) {
+  } else if (data instanceof ArrayBuffer) {
     return new Uint8Array(data)
-  }
-  else {
+  } else {
     return new Uint8Array(Buffer.from(data))
   }
 }
