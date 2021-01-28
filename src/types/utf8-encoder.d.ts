1dbd4fa0828cfc3b16918262eabde24810b65f84
Attila Gazso <agazso@gmail.com>
2021-01-28 12:48:02 +0100

    fix: remove utf8-encoder package dependency

--- src/types/utf8-encoder.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'utf8-encoder' {
-  function fromString(s: string): Uint8Array
-  function toString(u: Uint8Array): string
-}
--- src/utils/tar.ts
+++ src/utils/tar.ts
@@ -1,6 +1,5 @@
 import { Collection } from '../types'
 import Tar from 'tar-js'
-import * as utf8 from 'utf8-encoder'
 
 // this is a workaround type so that we are able to pass in Uint8Arrays
 // as string to `tar.append`
@@ -12,7 +11,7 @@ interface StringLike {
 // converts a string to utf8 Uint8Array and returns it as a string-like
 // object that `tar.append` accepts as path
 function fixUnicodePath(path: string): StringLike {
-  const codes = utf8.fromString(path)
+  const codes = new TextEncoder().encode(path)
 
   return {
     length: codes.length,
