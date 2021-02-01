02202e906950b4e67255a47f6fdd79abddfeba5c
Attila Gazso <agazso@gmail.com>
2021-02-01 19:06:59 +0100

    feat: uploadFile support for File type (#93)
    * feat: add File type support for uploadFile
    
    * refactor: remove ReadableStream support
    
    * refactor: simplify conditional
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,9 +1,11 @@
 import type { Readable } from 'stream'
 import type { Data } from 'ws'
 
-export function prepareData(data: string | Uint8Array | Readable): Uint8Array | Readable {
+export function prepareData(data: string | ArrayBuffer | Uint8Array | Readable): Uint8Array | Readable {
   if (typeof data === 'string') {
     return new TextEncoder().encode(data)
+  } else if (data instanceof ArrayBuffer) {
+    return new Uint8Array(data)
   }
 
   return data
---
+++ src/utils/file.ts
@@ -0,0 +1,40 @@
+/**
+ * Compatibility functions for working with File API objects
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/File
+ */
+
+export function isFile(file: unknown): file is File {
+  // browser
+  if (typeof File === 'function') {
+    return file instanceof File
+  }
+
+  // node.js
+  const f = file as File
+
+  return (
+    typeof f === 'object' &&
+    typeof f.name === 'string' &&
+    (typeof f.stream === 'function' || typeof f.arrayBuffer === 'function')
+  )
+}
+
+/**
+ * Compatibility helper for browsers where the `arrayBuffer function is
+ * missing from `File` objects.
+ *
+ * @param file A File object
+ */
+export function fileArrayBuffer(file: File): Promise<ArrayBuffer> {
+  if (file.arrayBuffer) {
+    return file.arrayBuffer()
+  }
+
+  // workaround for Safari where arrayBuffer is not supported on Files
+  return new Promise(resolve => {
+    const fr = new FileReader()
+    fr.onload = () => resolve(fr.result as ArrayBuffer)
+    fr.readAsArrayBuffer(file)
+  })
+}
