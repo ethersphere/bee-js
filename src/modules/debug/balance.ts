8aa3b7bb5cb8ac9760d8e0320d3b594f35b7b43b
Attila Gazso <agazso@gmail.com>
2021-02-01 19:13:00 +0100

    feat: balances and consumed endpoint

---
+++ src/modules/debug/balance.ts
@@ -0,0 +1,71 @@
+import { safeAxios } from "../../utils/safeAxios"
+
+const balancesEndpoint = '/balances'
+const consumedEndpoint = '/consumed'
+
+interface PeerBalance {
+  peer: string
+  balance: number
+}
+
+interface BalanceResponse {
+  balances: PeerBalance[]
+}
+
+/**
+ * Get the balances with all known peers including prepaid services
+ *
+ * @param url Bee debug url
+ */
+export async function getAllBalances(url: string): Promise<BalanceResponse> {
+  const response = await safeAxios<BalanceResponse>({
+    url: url + balancesEndpoint,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+/**
+ * Get the balances with a specific peer including prepaid services
+ *
+ * @param url     Bee debug url
+ * @param address Swarm address of peer
+ */
+export async function getPeerBalance(url: string, address: string): Promise<PeerBalance> {
+  const response = await safeAxios<PeerBalance>({
+    url: url + `${balancesEndpoint}/${address}`,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+/**
+ * Get the past due consumption balances with all known peers
+ *
+ * @param url Bee debug url
+ */
+export async function getPastDueConsumptionBalances(url: string): Promise<BalanceResponse> {
+  const response = await safeAxios<BalanceResponse>({
+    url: url + consumedEndpoint,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+/**
+ * Get the past due consumption balance with a specific peer
+ *
+ * @param url     Bee debug url
+ * @param address Swarm address of peer
+ */
+export async function getPastDueConsumptionPeerBalance(url: string, address: string): Promise<PeerBalance> {
+  const response = await safeAxios<PeerBalance>({
+    url: url + `${consumedEndpoint}/${address}`,
+    responseType: 'json',
+  })
+
+  return response.data
+}
