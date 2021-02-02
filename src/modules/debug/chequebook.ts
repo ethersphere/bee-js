97256f8654b37112ad6b4c949288ffb2cc1d08e8
Attila Gazso <agazso@gmail.com>
2021-02-02 13:13:45 +0100

    feat: chequebook api

---
+++ src/modules/debug/chequebook.ts
@@ -0,0 +1,177 @@
+import { safeAxios } from "../../utils/safeAxios"
+
+const chequebookEndpoint = '/chequebook'
+
+interface CheuqebookAddressResponse {
+  address: string
+}
+
+/**
+ * Get the address of the chequebook contract used
+ *
+ * @param url Bee debug url
+ */
+export async function getChequebookAddress(url: string): Promise<CheuqebookAddressResponse> {
+  const response = await safeAxios<CheuqebookAddressResponse>({
+    url: url + chequebookEndpoint + '/address',
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface ChequebookBalanceResponse {
+  totalBalance: number
+  availableBalance: number
+}
+
+/**
+ * Get the balance of the chequebook
+ *
+ * @param url Bee debug url
+ */
+export async function getChequeubookBalance(url: string): Promise<ChequebookBalanceResponse> {
+  const response = await safeAxios<ChequebookBalanceResponse>({
+    url: url + chequebookEndpoint + '/balance',
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface CashoutResult {
+  recipient: string
+  lastPayout: number
+  bounced: boolean
+}
+
+interface LastCashoutActionResponse {
+  peer: string
+  chequebook: string
+  cumulativePayout: number
+  beneficiary: string
+  transactionHash: string
+  result: CashoutResult
+}
+
+/**
+ * Get last cashout action for the peer
+ *
+ * @param url   Bee debug url
+ * @param peer  Swarm address of peer
+ */
+export async function getLastCashoutAction(url: string, peer: string): Promise<LastCashoutActionResponse> {
+  const response = await safeAxios<LastCashoutActionResponse>({
+    url: url + chequebookEndpoint + `/cashout/${peer}`,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface CashoutResponse {
+  transactionHash: string
+}
+
+/**
+ * Cashout the last cheque for the peer
+ *
+ * @param url   Bee debug url
+ * @param peer  Swarm address of peer
+ */
+export async function cashoutLastCheque(url: string, peer: string): Promise<CashoutResponse> {
+  const response = await safeAxios<LastCashoutActionResponse>({
+    method: 'post',
+    url: url + chequebookEndpoint + `/cashout/${peer}`,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface Cheque {
+  beneficiary: string
+  chequebook: string
+  payout: number
+}
+
+interface LastChequesForPeerResponse {
+  peer: string
+  lastreceived: Cheque
+  lastsent: Cheque
+}
+
+/**
+ * Get last cheques for the peer
+ *
+ * @param url   Bee debug url
+ * @param peer  Swarm address of peer
+ */
+export async function getLastChequesForPeer(url: string, peer: string): Promise<LastChequesForPeerResponse> {
+  const response = await safeAxios<LastChequesForPeerResponse>({
+    url: url + chequebookEndpoint + `/cheque/${peer}`,
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface LastChequesResponse {
+  lastcheques: LastChequesForPeerResponse[]
+}
+
+/**
+ * Get last cheques for all peers
+ *
+ * @param url   Bee debug url
+ */
+export async function getLastCheques(url: string): Promise<LastChequesResponse> {
+  const response = await safeAxios<LastChequesResponse>({
+    url: url + chequebookEndpoint + '/cheque',
+    responseType: 'json',
+  })
+
+  return response.data
+}
+
+interface DepositTokensResponse {
+  transactionHash: string
+}
+
+/**
+ * Deposit tokens from overlay address into chequebook
+ *
+ * @param url     Bee debug url
+ * @param amount  Amount of tokens to deposit
+ */
+export async function depositTokens(url: string, amount: number): Promise<DepositTokensResponse> {
+  const response = await safeAxios<DepositTokensResponse>({
+    method: 'post',
+    url: url + chequebookEndpoint + '/deposit',
+    responseType: 'json',
+    params: { amount },
+  })
+
+  return response.data
+}
+
+interface WithdrawTokensResponse {
+  transactionHash: string
+}
+
+/**
+ * Withdraw tokens from the chequebook to the overlay address
+ *
+ * @param url     Bee debug url
+ * @param amount  Amount of tokens to withdraw
+ */
+export async function withdrawTokens(url: string, amount: number): Promise<WithdrawTokensResponse> {
+  const response = await safeAxios<WithdrawTokensResponse>({
+    method: 'post',
+    url: url + chequebookEndpoint + '/withdraw',
+    responseType: 'json',
+    params: { amount },
+  })
+
+  return response.data
+}
---
+++ test/modules/debug/chequebook.spec.ts
@@ -0,0 +1,18 @@
+import { getChequebookAddress, getChequeubookBalance } from "../../../src/modules/debug/chequebook"
+import { isHexString } from "../../../src/utils/hex"
+import { beeDebugUrl } from "../../utils"
+
+describe('chequebook', () => {
+  test('address', async () => {
+    const response = await getChequebookAddress(beeDebugUrl())
+
+    expect(isHexString(response.address)).toBeTruthy()
+  })
+
+  test('balance', async () => {
+    const response = await getChequeubookBalance(beeDebugUrl())
+
+    expect(typeof response.availableBalance).toBe('number')
+    expect(typeof response.totalBalance).toBe('number')
+  })
+})
