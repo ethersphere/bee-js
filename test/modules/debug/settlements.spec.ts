2a8339a25919cd2f1bd93de0b5be56f977e5389b
Attila Gazso <agazso@gmail.com>
2021-02-03 12:16:01 +0100

    test: change getSettlement to be called conditionally

--- test/modules/debug/settlements.spec.ts
+++ test/modules/debug/settlements.spec.ts
@@ -1,29 +1,30 @@
-import * as connectivity from '../../../src/modules/debug/connectivity'
 import * as settlements from '../../../src/modules/debug/settlements'
-import { beeDebugUrl, beePeerUrl } from '../../utils'
-
-// helper function to get the peer overlay address
-async function getPeerOverlay() {
-  const nodeAddresses = await connectivity.getNodeAddresses(beeDebugUrl(beePeerUrl()))
-
-  return nodeAddresses.overlay
-}
+import { beeDebugUrl } from '../../utils'
 
 describe('settlements', () => {
-  test('peer settlements', async () => {
-    const peer = await getPeerOverlay()
-    const response = await settlements.getSettlements(beeDebugUrl(), peer)
-
-    expect(response.peer).toEqual(peer)
-    expect(typeof response.received).toBe('number')
-    expect(typeof response.sent).toBe('number')
-  })
-
   test('all settlements', async () => {
     const response = await settlements.getAllSettlements(beeDebugUrl())
 
-    expect(Array.isArray(response.settlements)).toBeTruthy()
     expect(typeof response.totalreceived).toBe('number')
     expect(typeof response.totalsent).toBe('number')
+    expect(response.settlements).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          peer: expect.any(String),
+          received: expect.any(Number),
+          sent: expect.any(Number),
+        }),
+      ]),
+    )
+
+    if (response.settlements.length > 0) {
+      const peerSettlement = response.settlements[0]
+
+      const peerSettlementResponse = await settlements.getSettlements(beeDebugUrl(), peerSettlement.peer)
+
+      expect(peerSettlementResponse.peer).toEqual(peerSettlement.peer)
+      expect(typeof peerSettlementResponse.received).toBe('number')
+      expect(typeof peerSettlementResponse.sent).toBe('number')
+    }
   })
 })
