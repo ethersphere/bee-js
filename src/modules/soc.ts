89577e15a66c427f5e9a5fa10c88ce3ad4829e7f
Attila Gazso <agazso@gmail.com>
2021-02-05 12:58:00 +0100

    feat: add /soc endpoint module

---
+++ src/modules/soc.ts
@@ -0,0 +1,39 @@
+import { ReferenceResponse, UploadOptions } from "../types"
+import { extractUploadHeaders } from "../utils/headers"
+import { safeAxios } from "../utils/safeAxios"
+
+const socEndpoint = '/soc'
+
+/**
+ * Upload single owner chunk (SOC) to a Bee node
+ *
+ * @param url         Bee URL
+ * @param owner       Owner's ethereum address in hex
+ * @param identifier  Arbitrary identifier in hex
+ * @param signature   Signature in hex
+ * @param data        Content addressed chunk data to be uploaded
+ * @param options     Aditional options like tag, encryption, pinning
+ */
+export async function upload(
+  url: string,
+  owner: string,
+  identifier: string,
+  signature: string,
+  data: Uint8Array,
+  options?: UploadOptions,
+): Promise<ReferenceResponse> {
+  console.debug({data})
+  const response = await safeAxios<ReferenceResponse>({
+    method: 'post',
+    url: `${url}${socEndpoint}/${owner}/${identifier}`,
+    data,
+    headers: {
+      'content-type': 'application/octet-stream',
+      ...extractUploadHeaders(options),
+    },
+    responseType: 'json',
+    params: { sig: signature }
+  })
+
+  return response.data
+}
