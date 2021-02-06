1f56844b53cf5108a51cdf62e02e627434a72bdb
nugaon <toth.viktor.levente@gmail.com>
2021-02-06 19:06:23 +0100

    fix: move test types to root 'type' folder and remove 'typeRoots' property from tsconfig

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,13 @@
-import * as TestTypes from '../test/types'
-export { TestTypes }
+export {} //indicate it is a module type declaration
+
+declare global {
+  interface Window {
+    BeeJs: typeof import('../src')
+  }
+  namespace jest {
+    interface Matchers<R> {
+      toBeHashReference(): R
+      toBeBeeResponse(statusCode: number): R
+    }
+  }
+}
