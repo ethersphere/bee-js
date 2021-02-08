5ab02207fed29c85ebe862adb944b5899d676507
nugaon <toth.viktor.levente@gmail.com>
2021-02-08 11:29:37 +0100

    fix: not-exposed project types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,6 @@
 export {} //indicate it is a module type declaration
 
 declare global {
-  interface Window {
-    BeeJs: typeof import('../src')
-  }
   namespace jest {
     interface Matchers<R> {
       toBeHashReference(): R
