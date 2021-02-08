884a76eee1813aae6d21d0dddb3f49bb62972ec6
nugaon <toth.viktor.levente@gmail.com>
2021-02-08 17:22:44 +0100

    revert: refactor: expose BeeError
    BeeError actually exposed from Utils

--- src/types/index.ts
+++ src/types/index.ts
@@ -94,5 +94,3 @@ export type BrandedType<T, N> = T & { __tag__: N }
 export type BrandedString<N> = BrandedType<string, N>
 
 export type FlavoredType<T, N> = T & { __tag__?: N }
-
-export { BeeError }
