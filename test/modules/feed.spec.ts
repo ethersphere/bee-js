23e51ef3d736f2ec15a545309bcff34922483352
Attila Gazso <agazso@gmail.com>
2021-02-15 12:25:17 +0100

    test: add test when feed update returns

--- test/modules/feed.spec.ts
+++ test/modules/feed.spec.ts
@@ -1,6 +1,7 @@
 import { createFeedManifest, findFeedUpdate } from '../../src/modules/feed'
-import { HexString, stripHexPrefix } from '../../src/utils/hex'
+import { HexString, hexToBytes, stripHexPrefix } from '../../src/utils/hex'
 import { beeUrl, testIdentity } from '../utils'
+import { upload as uploadSOC } from '../../src/modules/soc'
 
 describe('modules/feed', () => {
   const url = beeUrl()
@@ -19,5 +20,23 @@ describe('modules/feed', () => {
     const feedUpdate = findFeedUpdate(url, owner, emptyTopic)
 
     await expect(feedUpdate).rejects.toThrow('Not Found')
+  }, 15000)
+
+  test('one feed update', async () => {
+    const oneUpdateTopic = '2000000000000000000000000000000000000000000000000000000000000000'
+    const identifier = '7c5c4c857ed4cae434c2c737bad58a93719f9b678647310ffd03a20862246a3b'
+    const signature =
+      'bba40ea2c87b7801f54f5cca70e06deaed5c366b588e38ce0c42f7f8f16562c3243b43101faa6dbaeaab3244b1a0ceaec92dd117995e19116a372eadbec945b01b'
+    const socData = hexToBytes(
+      '280000000000000000000000602a57df0000000000000000000000000000000000000000000000000000000000000000' as HexString,
+    )
+
+    const socResponse = await uploadSOC(url, owner, identifier, signature, socData)
+    expect(typeof socResponse.reference).toBe('string')
+
+    const feedUpdate = await findFeedUpdate(url, owner, oneUpdateTopic)
+    expect(typeof feedUpdate.reference).toBe('string')
+    expect(feedUpdate.feedIndex).toEqual('0000000000000000')
+    expect(feedUpdate.feedIndexNext).toEqual('0000000000000001')
   }, 15000)
 })
