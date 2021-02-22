f8feab59432498ec9b72654433f2ef4d92a924ef
Attila Gazso <agazso@gmail.com>
2021-02-22 11:01:41 +0100

    refactor: change FeedType without repeating string literal

--- src/feed/type.ts
+++ src/feed/type.ts
@@ -1,9 +1,8 @@
-export type FeedType = 'sequence' | 'epoch'
-
-const feedTypes = ['sequence', 'epoch']
+const feedTypes = ['sequence', 'epoch'] as const
+export type FeedType = typeof feedTypes[number]
 
 export function isFeedType(type: unknown): type is FeedType {
-  return typeof type === 'string' && feedTypes.includes(type)
+  return typeof type === 'string' && feedTypes.includes(type as FeedType)
 }
 export function assertIsFeedType(type: unknown): asserts type is FeedType {
   if (!isFeedType(type)) {
