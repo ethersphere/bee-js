a5136590aa2de788ba1ab8f55f762f5e56f0f790
Attila Gazso <agazso@gmail.com>
2021-02-26 11:53:58 +0100

    test: increase timeout for feed update & lookup test

--- test/bee-class.spec.ts
+++ test/bee-class.spec.ts
@@ -230,7 +230,9 @@ describe('Bee class', () => {
 
       expect(secondUpdateReferenceResponse.reference).toEqual(bytesToHex(referenceOne))
       expect(secondUpdateReferenceResponse.feedIndex).toEqual('0000000000000001')
-    }, 60000)
+      // TODO the timeout was increased because this test is flaky
+      // most likely there is an issue with the lookup
+    }, 120000)
 
     describe('topic', () => {
       test('create feed topic', () => {
