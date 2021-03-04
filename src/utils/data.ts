c9aadb0ef58f24f12c339efa3f024f979bd942fa
Vojtech Simetka <vojtech@simetka.cz>
2021-03-04 10:08:28 +0100

    fix: pss subscribe in browsers

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -11,14 +11,22 @@ export function prepareData(data: string | ArrayBuffer | Uint8Array | Readable):
   return data
 }
 
-export function prepareWebsocketData(data: Data): Uint8Array {
+function isBufferArray(data: unknown) {
+  return Array.isArray(data) && data.length > 0 && data.reduce((p, c) => p && c instanceof Buffer, true)
+}
+
+export async function prepareWebsocketData(data: Data | Blob): Promise<Uint8Array> | never {
   if (typeof data === 'string') {
     return new TextEncoder().encode(data)
   } else if (data instanceof Buffer) {
     return new Uint8Array(data)
   } else if (data instanceof ArrayBuffer) {
     return new Uint8Array(data)
-  } else {
+  } else if (data instanceof Blob) {
+    return new Uint8Array(await new Response(data as Blob).arrayBuffer())
+  } else if (isBufferArray(data)) {
     return new Uint8Array(Buffer.concat(data))
   }
+
+  throw new TypeError('unknown websocket data type')
 }
