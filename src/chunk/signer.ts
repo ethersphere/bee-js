e44a13ac8af4561b9ae19dcd7e02e84bcbdcce1d
Adam Uhlíř <adam@uhlir.dev>
2021-03-09 12:15:57 +0100

    docs: expand signer interface documentation

--- src/chunk/signer.ts
+++ src/chunk/signer.ts
@@ -22,6 +22,11 @@ type AsyncSigner = (digest: Uint8Array) => Promise<Signature>
  * In order to be compatible with Ethereum and its signing method `personal_sign`, the data
  * that are passed to sign() function should be prefixed with: `\x19Ethereum Signed Message:\n${data.length}`, hashed
  * and only then signed.
+ * If you are wrapping another signer tool/library (like Metamask or some other Ethereum wallet), you might not have
+ * to do this prefixing manually if you use the `personal_sign` method. Check documentation of the tool!
+ * If you are writing your own storage for keys, then you have to prefix the data manually otherwise the Bee node
+ * will reject the chunk!
+ *
  * For example see the hashWithEthereumPrefix() function.
  *
  * @property sign     The sign function that can be sync or async. This function takes non-prefixed data. See above.
