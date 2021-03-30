1b0df0dea0b73b9f991587b0d9bd6dc4812e121b
Vojtech Simetka <vojtech@simetka.cz>
2021-03-30 13:27:40 +0200

    feat: add supported bee version to package.json

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { BeeDebug } from './bee-debug'
 export * as Utils from './utils/expose'
 export * from './types'
 export * from './utils/error'
+export { SUPPORTED_BEE_VERSION, SUPPORTED_BEE_VERSION_EXACT } from './modules/debug/status'
 export { Bee, BeeDebug }
 
 // for requrie-like imports
