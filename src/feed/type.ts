b874a3f67bbb9fb1475e0d65a2c28320fadb098a
Adam Uhlíř <adam@uhlir.dev>
2021-05-06 10:20:53 +0200

    refactor: assert guards unification

--- src/feed/type.ts
+++ src/feed/type.ts
@@ -5,7 +5,7 @@ export const DEFAULT_FEED_TYPE: FeedType = 'sequence'
 export function isFeedType(type: unknown): type is FeedType {
   return typeof type === 'string' && feedTypes.includes(type as FeedType)
 }
-export function assertIsFeedType(type: unknown): asserts type is FeedType {
+export function assertFeedType(type: unknown): asserts type is FeedType {
   if (!isFeedType(type)) {
     throw new TypeError('invalid feed type')
   }
