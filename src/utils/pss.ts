85ad4cbe7b1141de62dd645b4cc7e359ff758657
Adam Uhlíř <adam@uhlir.dev>
2021-09-01 10:00:43 +0200

    docs: target param

--- src/utils/pss.ts
+++ src/utils/pss.ts
@@ -4,7 +4,7 @@ import { AddressPrefix, PSS_TARGET_HEX_LENGTH_MAX } from '../types'
  * Utility function that for given strings/reference takes the most specific
  * target that Bee node will except.
  *
- * @param target
+ * @param target is a non-prefixed hex string Bee address
  * @see [Bee docs - PSS](https://docs.ethswarm.org/docs/dapps-on-swarm/pss)
  */
 export function makeMaxTarget(target: string): AddressPrefix {
