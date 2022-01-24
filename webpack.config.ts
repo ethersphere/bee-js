7d84396af2a9de091d299a607d5d4e7f4e3f8044
Adam Uhlíř <adam@uhlir.dev>
2022-01-24 08:27:34 +0100

    feat: hybrid esm build

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,10 @@
 /* eslint-disable no-console */
 import Path from 'path'
 import TerserPlugin from 'terser-webpack-plugin'
-import { Configuration, DefinePlugin, NormalModuleReplacementPlugin, WebpackPluginInstance } from 'webpack'
+import { Configuration, DefinePlugin, WebpackPluginInstance } from 'webpack'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-import { getBrowserPathMapping } from './jest.config'
-import PackageJson from './package.json'
 
 interface WebpackEnvParams {
-  target: 'web' | 'node'
   debug: boolean
   mode: 'production' | 'development'
   fileName: string
@@ -15,37 +12,15 @@ interface WebpackEnvParams {
 
 const base = async (env?: Partial<WebpackEnvParams>): Promise<Configuration> => {
   const isProduction = env?.mode === 'production'
-  const isBrowser = env?.target === 'web'
-  const filename =
-    env?.fileName ||
-    ['index', isBrowser ? '.browser' : null, isProduction ? '.min' : null, '.js'].filter(Boolean).join('')
+  const filename = env?.fileName || ['index', isProduction ? '.min' : null, '.js'].filter(Boolean).join('')
   const entry = Path.resolve(__dirname, 'src')
   const path = Path.resolve(__dirname, 'dist')
-  const target = env?.target || 'web' // 'node' or 'web'
   const plugins: WebpackPluginInstance[] = [
     new DefinePlugin({
       'process.env.ENV': env?.mode || 'development',
       'process.env.IS_WEBPACK_BUILD': 'true',
     }),
   ]
-
-  if (target === 'web') {
-    const browserPathMapping = await getBrowserPathMapping()
-    // eslint-disable-next-line guard-for-in
-    for (const nodeReference in browserPathMapping) {
-      plugins.push(
-        new NormalModuleReplacementPlugin(new RegExp(`\\${nodeReference}`), browserPathMapping[nodeReference]),
-      )
-    }
-    // change node modules to browser modules according to packageJson.browser mapping
-    // eslint-disable-next-line guard-for-in
-    const browserModuleMapping = PackageJson.browser as { [key: string]: string }
-    // eslint-disable-next-line guard-for-in
-    for (const nodeReference in browserModuleMapping) {
-      const browserReference: string = browserModuleMapping[nodeReference]
-      plugins.push(new NormalModuleReplacementPlugin(new RegExp(`^${nodeReference}$`), browserReference))
-    }
-  }
 
   return {
     bail: Boolean(isProduction),
@@ -109,11 +84,6 @@ const base = async (env?: Partial<WebpackEnvParams>): Promise<Configuration> => 
     },
     plugins,
     target,
-    node: {
-      global: true,
-      __filename: 'mock',
-      __dirname: 'mock',
-    },
     performance: {
       hints: false,
     },
