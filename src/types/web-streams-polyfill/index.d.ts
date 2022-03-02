cda5cb49ea0971e66e4fdb8e07e364004f14dfa5
Adam Uhlíř <adam@uhlir.dev>
2022-03-02 12:39:36 +0100

    fix: web-stream-polyfill typings

--- src/types/web-streams-polyfill/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This is type "alias" for the direct import of the web-streams-polyfill/dist/ponyfill.js
- */
-declare module 'web-streams-polyfill/dist/ponyfill.js' {
-  export * from 'web-streams-polyfill/ponyfill'
-}
