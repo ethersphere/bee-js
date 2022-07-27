6711dff47bdce9f76126d15ef3815237211b9907
Adam Uhlíř <adam@uhlir.dev>
2022-07-27 16:16:13 +0200

    refactor!: drop DebugPostageBatch in favor of PostageBatch inteface

--- src/types/index.ts
+++ src/types/index.ts
@@ -500,11 +500,6 @@ export interface PostageBatch {
   exists: boolean
 }
 
-/**
- * @deprecated Use PostageBatch instead.
- */
-export type DebugPostageBatch = PostageBatch
-
 export interface BatchBucket {
   bucketID: number
   collisions: number
