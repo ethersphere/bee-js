d9a87c8a78a75711121684363e15fc0ce33ba6a8
bee-worker <bee-worker@ethswarm.org>
2023-02-07 13:32:44 +0000

    chore: update to bee

--- src/modules/debug/status.ts
+++ src/modules/debug/status.ts
@@ -6,7 +6,7 @@ import getMajorSemver from 'semver/functions/major.js'
 
 // Following lines bellow are automatically updated with GitHub Action when Bee version is updated
 // so if you are changing anything about them change the `update_bee` action accordingly!
-export const SUPPORTED_BEE_VERSION_EXACT = '1.12.0-rc2-83d8d333'
+export const SUPPORTED_BEE_VERSION_EXACT = '1.12.0-rc3-4360deea'
 export const SUPPORTED_API_VERSION = '4.0.0'
 export const SUPPORTED_DEBUG_API_VERSION = '4.0.0'
 
