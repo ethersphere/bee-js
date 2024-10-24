8e9116b8fd30e1df4c6118068f933f32b983c4b3
Cafe137 <xcafe137@gmail.com>
2024-10-24 15:53:49 +0200

    style: code style

--- src/utils/cid.ts
+++ src/utils/cid.ts
@@ -21,18 +21,24 @@ export function convertReferenceToCid(reference: Reference | string, type: 'feed
   const unknown = new Uint8Array([1])
   const sha256 = new Uint8Array([27])
   const size = new Uint8Array([32])
-  const header = Binary.uint8ArrayToBase32(Binary.concatBytes(version, codec, unknown, sha256, size)).replace(/=+$/, '')
-  const hash = Binary.uint8ArrayToBase32(Binary.hexToUint8Array(reference)).replace(/=+$/, '')
+  const header = Binary.uint8ArrayToBase32(Binary.concatBytes(version, codec, unknown, sha256, size)).replace(
+    /\=+$/,
+    '',
+  )
+  const hash = Binary.uint8ArrayToBase32(Binary.hexToUint8Array(reference)).replace(/\=+$/, '')
+
   return `${base32}${header}${hash}`.toLowerCase()
 }
 
 export function convertCidToReference(cid: string): DecodedCID {
   const bytes = Binary.base32ToUint8Array(cid.toLowerCase())
   const codec = bytes[2]
+
   if (!CODEC_TABLE[codec]) {
     throw new Error('Unknown codec')
   }
   const reference = bytes.slice(-32)
+
   return {
     type: CODEC_TABLE[codec] as 'feed' | 'manifest',
     reference: Binary.uint8ArrayToHex(reference) as Reference,
