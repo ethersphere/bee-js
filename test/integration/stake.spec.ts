aef22cde1906ec81eacab1a922508ea107b19b0f
Cafe137 <xcafe137@gmail.com>
2025-02-13 23:16:43 +0100

    test: test passing bzz instance to deposit stake

--- test/integration/stake.spec.ts
+++ test/integration/stake.spec.ts
@@ -1,3 +1,5 @@
+import { Dates, System } from 'cafe-utility'
+import { BZZ } from '../../src'
 import { currentBeeMode, makeBee } from '../utils'
 
 const bee = makeBee()
@@ -17,6 +19,29 @@ test('POST stake', async () => {
     return
   }
 
-  const transactionId = await bee.depositStake('1')
+  let stakePreviously = await bee.getStake()
+
+  let transactionId = await bee.depositStake('1')
   expect(transactionId.toHex()).toHaveLength(64)
+
+  await System.waitFor(
+    async () => {
+      const stake = await bee.getStake()
+      return stake.eq(stakePreviously.plus('1'))
+    },
+    Dates.seconds(1),
+    180,
+  )
+
+  transactionId = await bee.depositStake(BZZ.fromPLUR(1n))
+  expect(transactionId.toHex()).toHaveLength(64)
+
+  await System.waitFor(
+    async () => {
+      const stake = await bee.getStake()
+      return stake.eq(stakePreviously.plus('2'))
+    },
+    Dates.seconds(1),
+    180,
+  )
 })
