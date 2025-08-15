b634a1c73258327fbff7fc81675d5f4a3829d996
Cafe137 <xcafe137@gmail.com>
2025-08-15 15:13:39 +0200

    test: compare coverage

--- jest.config.ts
+++ jest.config.ts
@@ -30,7 +30,7 @@ export default async (): Promise<Config.InitialOptions> => {
   return {
     collectCoverage: true,
     coverageDirectory: 'coverage',
-    coverageReporters: ['lcov'],
+    coverageReporters: ['lcov', 'json-summary'],
     collectCoverageFrom: ['src/**/*.ts'],
     moduleDirectories: ['node_modules'],
     projects: [
