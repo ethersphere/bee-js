224a7bc9defff27faf8623232fd4f465c5db52e2
Cafe137 <xcafe137@gmail.com>
2026-05-15 16:18:27 +0200

    test: merge get/post stake tests

--- test/integration/stake.spec.ts
+++ test/integration/stake.spec.ts
@@ -1,46 +1,20 @@
 import { Dates, System } from 'cafe-utility'
 import { BZZ } from '../../src'
-import { currentBeeMode, makeBee } from '../utils'
+import { makeBee } from '../utils'
 
 const bee = makeBee()
 
-test('GET stake', async () => {
-  const stake = await bee.getStake()
-
-  if (currentBeeMode() === 'full') {
-    expect(parseFloat(stake.toDecimalString())).toBeGreaterThan(10)
-  } else {
-    expect(parseFloat(stake.toDecimalString())).toBe(0)
-  }
-})
-
-test('POST stake', async () => {
-  if (currentBeeMode() !== 'full') {
-    return
-  }
-
+test('GET/POST stake', async () => {
   const stakePreviously = await bee.getStake()
 
-  let transactionId = await bee.depositStake('1')
+  const transactionId = await bee.depositStake(BZZ.fromFloat(10))
   expect(transactionId.toHex()).toHaveLength(64)
 
   await System.waitFor(
     async () => {
       const stake = await bee.getStake()
 
-      return stake.eq(stakePreviously.plus('1'))
-    },
-    { attempts: 180, waitMillis: Dates.seconds(1) },
-  )
-
-  transactionId = await bee.depositStake(BZZ.fromPLUR(1n))
-  expect(transactionId.toHex()).toHaveLength(64)
-
-  await System.waitFor(
-    async () => {
-      const stake = await bee.getStake()
-
-      return stake.eq(stakePreviously.plus('2'))
+      return stake.eq(stakePreviously.plus(BZZ.fromFloat(10)))
     },
     { attempts: 180, waitMillis: Dates.seconds(1) },
   )
