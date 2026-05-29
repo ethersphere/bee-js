2b2375f2f78266e172f7f86bbb04606b53f19277
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-05-29 12:31:43 +0200

    fix: update response parsing

--- src/modules/debug/stamps.ts
+++ src/modules/debug/stamps.ts
@@ -1,12 +1,12 @@
 import { Types } from 'cafe-utility'
 import type {
-  BeeRequestOptions,
-  GlobalPostageBatch,
-  NumberString,
-  PostageBatch,
-  PostageBatchBuckets,
-  PostageBatchOptions,
-  RedundancyLevel,
+    BeeRequestOptions,
+    GlobalPostageBatch,
+    NumberString,
+    PostageBatch,
+    PostageBatchBuckets,
+    PostageBatchOptions,
+    RedundancyLevel,
 } from '../../types'
 import { http } from '../../utils/http'
 import { mapPostageBatch, RawPostageBatch } from '../../utils/stamps'
@@ -162,5 +162,6 @@ function validateRawPostageBatch(raw: Record<string, unknown>): RawPostageBatch 
     label: Types.asEmptiableString(raw.label, { name: 'label' }),
     usable: Types.asBoolean(raw.usable, { name: 'usable' }),
     utilization: Types.asNumber(raw.utilization, { name: 'utilization' }),
+    utilizationRatio: Types.asNumber(raw.utilization, { name: 'utilizationRatio' }),
   }
 }
