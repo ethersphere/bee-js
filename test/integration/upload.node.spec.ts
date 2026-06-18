735aaee0d5c6283946291bcb6f3cde7d91cb98c9
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-06-18 14:48:54 +0200

    test: update hashes :crossed_fingers:

--- test/integration/upload.node.spec.ts
+++ test/integration/upload.node.spec.ts
@@ -4,7 +4,7 @@ import { batch, makeBee } from '../utils'
 const bee = makeBee()
 
 test('upload files from directory', async () => {
-  const expectedHash = '237865537469cc454a0d2d8ae913b1402f360af045d956caccf1f1724f597118'
+  const expectedHash = '32c8aa1c32d3ba4ded3dbc6df75d7a3a58b6468c6227fe721af06365f769a8f7'
 
   // use bzz api with streaming tar
   const response = await bee.uploadFilesFromDirectory(batch(), 'test/data')
@@ -17,7 +17,7 @@ test('upload files from directory', async () => {
 
   // check directory hash locally
   const hash = await bee.hashDirectory('test/data')
-  expect(hash.toHex()).toBe(expectedHash)
+  expect(hash.toHex()).toBe('237865537469cc454a0d2d8ae913b1402f360af045d956caccf1f1724f597118')
 
   // stream chunks to upload
   const streamResponse = await bee.streamDirectory(batch(), 'test/data')
