495b8b66081491862592b2029d42b50ab31abdf1
Gergely Békési <gergely.bekesi@ethswarm.org>
2026-06-19 16:52:04 +0200

    fix: regex

--- src/utils/hex-schema.ts
+++ src/utils/hex-schema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 
 export const HexStringSchema = z
   .string()
-  .regex(/^(?:0x)?[0-9a-fA-F]*$/, 'expected hex string')
+  .regex(/^(?:0x)?[0-9a-fA-F]*$/i, 'expected hex string')
   .refine(s => s.replace(/^0x/i, '').length % 2 === 0, 'expected even-length hex string')
